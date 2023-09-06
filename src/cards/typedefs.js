df0f2b55680f1822a7b18def0506b80eac38a9bd
Aalian Khan <39784853+AalianKhan@users.noreply.github.com>
2023-09-06 16:22:02 -0400

    Add LockCardOptions to typedef.js

--- src/cards/typedefs.js
+++ src/cards/typedefs.js
@@ -56,6 +56,12 @@
  */
 
 /**
+ * @typedef {abstractOptions & Object} lockCardOptions Lock Card options.
+ * @property {{tap_action: switchTapAction}} [action] Home assistant action to perform on tap.
+ * @memberOf typedefs.cards
+ */
+
+/**
  * @typedef {abstractOptions & Object} switchCardOptions Switch Card options.
  * @property {{tap_action: switchTapAction}} [action] Home assistant action to perform on tap.
  * @memberOf typedefs.cards
