ef0da37bf09461650446a54d2a8e3457fa17f5ad
DigiLive <github@digilive.nl>
2024-12-14 09:06:28 +0100

    Cut adding controller-card
    Since the controller card doesn't control anything, it has no use to
    add it to the view.

--- src/views/SceneView.ts
+++ src/views/SceneView.ts
@@ -1,8 +1,6 @@
 import {Helper} from "../Helper";
-import {ControllerCard} from "../cards/ControllerCard";
 import {AbstractView} from "./AbstractView";
 import {views} from "../types/strategy/views";
-import {cards} from "../types/strategy/cards";
 
 // noinspection JSUnusedGlobalSymbols Class is dynamically imported.
 /**
@@ -40,17 +38,6 @@ class SceneView extends AbstractView {
   };
 
   /**
-   * Default configuration of the view's Controller card.
-   *
-   * @type {cards.ControllerCardOptions}
-   * @private
-   */
-  #viewControllerCardConfig: cards.ControllerCardOptions = {
-    title: "All Scenes",
-    subtitle: Helper.getCountTemplate(SceneView.#domain, "match", ".*") + " scenes found",
-  };
-
-  /**
    * Class constructor.
    *
    * @param {views.ViewConfig} [options={}] Options for the view.
@@ -59,14 +46,6 @@ class SceneView extends AbstractView {
     super(SceneView.#domain);
 
     this.config = Object.assign(this.config, this.#defaultConfig, options);
-
-    // Create a Controller card to switch all entities of the domain.
-    this.viewControllerCard = new ControllerCard(
-      this.targetDomain(SceneView.#domain),
-      {
-        ...this.#viewControllerCardConfig,
-        ...("controllerCardOptions" in this.config ? this.config.controllerCardOptions : {}) as cards.ControllerCardConfig,
-      }).createCard();
   }
 }
 
